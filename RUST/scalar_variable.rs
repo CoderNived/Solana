a00eca251b063f5b431ae7fcd53c9dfec10c4e77
CoderNived <nivedshenoy0@gmail.com>
2026-01-21 06:34:14 +0530

    updating code

---
+++ RUST/scalar_variable.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let unsigned: u8=10;
+    let signed: i8=-20;
+    let float: f32=1.2;
+    println!("unsigned {}, signed {}, float {}", unsigned, signed, float);
+    let letter = "c12234";
+    let emoji = "\u{1F600}";
+    println!("letter {}, emoji {}", letter, emoji);
+    let is_true: bool=true;
+    println!("is_true {}", is_true)
+
+}
